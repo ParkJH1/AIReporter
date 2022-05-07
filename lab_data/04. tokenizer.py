5680e91e6e2a3c6de1545d1d680819cc2b07de8a
박정현 <pjh9996@naver.com>
2022-05-07 14:58:37 +0900

    Update 04. tokenizer.py

--- lab_data/04. tokenizer.py
+++ lab_data/04. tokenizer.py
@@ -5,9 +5,8 @@ data = pd.read_csv('../data/titles.csv')
 titles = data['title'].values
 
 tokenizer = tf.keras.preprocessing.text.Tokenizer()
+print(tokenizer.word_index)
 tokenizer.fit_on_texts(titles)
-
 word_count = len(tokenizer.word_index) + 1
-
 print(tokenizer.word_index)
 print(word_count)
